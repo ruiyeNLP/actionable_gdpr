d9405bfc08bd380e87d074e77e2cc1a557843a18
ruiyeNLP <86205554+ruiyeNLP@users.noreply.github.com>
2022-04-11 19:24:50 +0200

    Update keywords.py

--- event_extraction/keywords.py
+++ event_extraction/keywords.py
@@ -60,11 +60,8 @@ def analyse(file):
             word_dict[key].append(word[0])      
     word_dict = {}
     word_count(corpus)
-    #word_count(corpus, type='NOUN')
-    #word_count(corpus, type='VERB')
-    #bigramwords_list = word_count(corpus, ngram=2)
-    #for i in bigramwords_list:
-    #    word_dict['word_count'].append(i[0])
+    word_count(corpus, type='NOUN')
+    word_count(corpus, type='VERB')
 
     # tf-idf for weights to find similar words; 
     def tfidf(corpus,type=None,top_n=20):
@@ -81,9 +78,9 @@ def analyse(file):
             word_dict[key] = []
         for word in words:
             word_dict[key].append(word[0])        
-    #tfidf(corpus)
-    #tfidf(corpus, type='NOUN')
-    #tfidf(corpus, type='VERB')
+    tfidf(corpus)
+    tfidf(corpus, type='NOUN')
+    tfidf(corpus, type='VERB')
 
     # topic modeling -- get top words in each topic 
     def topic_modeling(corpus,colname,type=None,top_n=20):
@@ -106,28 +103,10 @@ def analyse(file):
         data_lda = lda.fit_transform(data_vectorized)
         print("LDA Model")
         selected_topics(lda, vectorizer)
-        # # Keywords for topics clustered by Latent Semantic Indexing
-        # nmf = NMF(n_components=NUM_TOPICS)
-        # data_nmf = nmf.fit_transform(data_vectorized) 
-        # print("NMF Model")
-        # selected_topics(nmf, vectorizer) 
 
-        # # Keywords for topics clustered by Non-Negative Matrix Factorization
-        # lsi = TruncatedSVD(n_components=NUM_TOPICS)
-        # data_lsi = lsi.fit_transform(data_vectorized)             
-        # print("LSI Model")
-        # selected_topics(lsi, vectorizer)
-
-        # # LDA for bigram data
-        # bivectorizer = CountVectorizer(min_df=5, max_df=0.9, stop_words='english', lowercase=True, ngram_range=(2,2))
-        # bigram_vectorized = bivectorizer.fit_transform(sentences)
-        # bi_lda = LatentDirichletAllocation(n_components=NUM_TOPICS, max_iter=10, learning_method='online',verbose=True)
-        # data_bi_lda = bi_lda.fit_transform(bigram_vectorized)
-        # print("Bi-LDA Model:")
-        # selected_topics(bi_lda, bivectorizer)
-    #topic_modeling(corpus, colname='lda_')
-    #topic_modeling(corpus, colname='lda_', type='NOUN')
-    #topic_modeling(corpus, colname='lda_', type='VERB')
+    topic_modeling(corpus, colname='lda_')
+    topic_modeling(corpus, colname='lda_', type='NOUN')
+    topic_modeling(corpus, colname='lda_', type='VERB')
 
     df = pd.DataFrame(word_dict)
     filename = './output/'+str(file).split('/')[-1].split('.')[0]+'_keywords'+'.csv'
@@ -137,18 +116,3 @@ file = "./input/contact_details.tsv"
 analyse(file)
 file = './input/user_rights_sentences.tsv'
 analyse(file)
-
-# #word embedding to enlarge similar words in doc 
-# def most_similar(word, topn=20):
-#     if not nlp(word)[0].is_alpha:
-#         print(word, 'is not alpha')
-#         return []
-#     ms = nlp.vocab.vectors.most_similar(
-#     np.asarray([nlp.vocab.vectors[nlp.vocab.strings[word]]]), n=topn)
-#     words = [nlp.vocab.strings[w] for w in ms[0][0]]
-#     distances = ms[2]  
-#     return words
-# for noun in common_nouns:
-#     print(noun[0], most_similar(noun[0]))
-# for verb in common_verbs:
-#     print(verb[0], most_similar(verb[0]))
