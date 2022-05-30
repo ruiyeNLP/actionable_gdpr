1de2e8e32fe79d60b9617b946de88fe69073c4ec
ruiyeNLP <86205554+ruiyeNLP@users.noreply.github.com>
2022-05-30 21:13:38 +0200

    Add files via upload
    preprocess step 1: filter user rights related sentences from corpus

---
+++ event_extraction/preprocess_URsentences_filter.py
@@ -0,0 +1,9 @@
+import csv
+with open('data.tsv','r') as fin,open('user_rights_sentences.tsv','w') as fout:
+    reader = csv.reader(fin,delimiter='\t')
+    writer = csv.writer(fout,delimiter='\t')
+    #writer.writerow(["label", "review", "filename"])
+    #writer.writeheader()
+    for row in reader:
+        if row[0] == '10' or row[0] == '9' or row[0] == '8' or row[0] == '7' or row[0] == '6' or row[0] == '5' or row[0] == '4':
+            writer.writerow(row)
